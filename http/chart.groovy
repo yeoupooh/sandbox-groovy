ff79202ec660c0fa07a257edd73dd3006a056231
Thomas Min <thomas.min.git@gmail.com>
2015-06-09 22:20:56 +0900

    Update closure call

--- http/chart.groovy
+++ http/chart.groovy
@@ -1,1 +1,6 @@
-evaluate(new File("melon.groovy"))
+// Refer to http://www.programcreek.com/java-api-examples/index.php?api=groovy.lang.GroovyShell
+def shell = new GroovyShell()
+def code = new File("melon.groovy").text
+def closure = shell.evaluate(code)
+def output = closure("tracks", null)
+println output
--- http/melon.groovy
+++ http/melon.groovy
@@ -1,8 +1,16 @@
-def html = new URL("http://www.melon.com/chart/")
-        .getText(connectTimeout: 5000,
-        readTimeout: 10000,
-        useCaches: true,
-        allowUserInteraction: false,
-        requestProperties: ['Connection': 'close'])
+{ it, args ->
+    def getTracks = {
+        def html = new URL("http://www.melon.com/chart/")
+                .getText(connectTimeout: 5000,
+                readTimeout: 10000,
+                useCaches: true,
+                allowUserInteraction: false,
+                requestProperties: ['Connection': 'close'])
+        return html
+    }
 
-println html
+    if (it == "tracks") {
+        return getTracks()
+    }
+}
+
