831a1dd2872898a9722093c219ad5616d039473f
Thomas Min <thomas.min.git@gmail.com>
2015-07-06 15:46:42 +0900

    Moved HttpClientHelper from TelegramBotApiHelper class.
    Make simple_boot runnable.

---
+++ telegram/HttpClientHelper.groovy
@@ -0,0 +1,106 @@
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.ssl.SSLSocketFactory
+
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+
+import static groovyx.net.http.Method.HEAD
+
+@Grapes([
+        @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.1'),
+        @Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = '4.5')
+])
+
+/**
+ *
+ */
+public class HttpClientHelper {
+
+/**
+ *
+ * @param url
+ * @return
+ */
+    public static String getUrl(url) {
+        println "url=" + url
+        def http = new HTTPBuilder(url)
+
+// https://github.com/jgritman/httpbuilder/blob/master/src/test/groovy/groovyx/net/http/SSLTest.groovy
+// http://humbleprogrammer.wikidot.com/groovy-ssl
+// http://stackoverflow.com/questions/11638005/is-there-an-easier-way-to-tell-httpbuilder-to-ignore-an-invalid-cert
+//import org.apache.http.conn.scheme.Scheme
+//import org.apache.http.conn.ssl.SSLSocketFactory
+//
+//import javax.net.ssl.SSLContext
+//import javax.net.ssl.TrustManager
+//import javax.net.ssl.X509TrustManager
+//import java.security.SecureRandom
+//import java.security.cert.CertificateException
+//import java.security.cert.X509Certificate
+//
+//import static groovyx.net.http.Method.HEAD
+//=== SSL UNSECURE CERTIFICATE ===
+        def sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, [new X509TrustManager() {
+            void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                System.out.println("checkClientTrusted =============")
+            }
+
+            void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                System.out.println("checkServerTrusted =============")
+            }
+
+            X509Certificate[] getAcceptedIssuers() {
+                System.out.println("getAcceptedIssuers =============")
+                return null
+            }
+        }] as TrustManager[], new SecureRandom())
+
+        def sf = new SSLSocketFactory(sslContext, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+        def httpsScheme = new Scheme("https", sf, 443)
+        http.client.connectionManager.schemeRegistry.register(httpsScheme)
+//================================
+
+//// https://github.com/jgritman/httpbuilder/wiki/SSL
+// -----------------------------------
+//import org.apache.http.conn.scheme.*
+//import org.apache.http.conn.ssl.*
+//import java.security.*
+//import javax.net.ssl.*
+//def keyStore = KeyStore.getInstance(KeyStore.defaultType)
+//
+//getClass().getResource(config.keystoreResource).withInputStream {
+//    keyStore.load(it, config.keystorePassword.toCharArray())
+//}
+//
+//final socketFactory = new SSLSocketFactory(keyStore)
+//socketFactory.hostnameVerifier = SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER
+//
+//http.client.connectionManager.schemeRegistry.register(
+//        new Scheme("https", socketFactory, 443))
+// -----------------------------------
+
+// HTTP Request
+        println "HEAD:" + HEAD.toString()
+        def result
+        def status = http.request(Method.GET, ContentType.TEXT) {
+            response.success = { resp, reader ->
+                println "response status: ${resp.statusLine}"
+                result = new String(reader.text)
+            }
+            response.failure = {
+                println "statusCode:$it.statusLine.statusCode"
+            }
+        }
+
+        return result
+    }
+
+}
--- telegram/simple_bot.groovy
+++ telegram/simple_bot.groovy
@@ -3,14 +3,34 @@ import groovy.json.JsonSlurper
 def config = new JsonSlurper().parseText(new File("./telegram.bot.config.json").text)
 String token = config.token
 
-def updates = TelegramBotApiHelper.getUpdates(token)
-println "getUpdates:" + updates
+def prevLastMsg
+def offset = 0
+String R_CMD = "/r "
+Thread.start {
+    while (true) {
+        def updates = TelegramBotApiHelper.getUpdates(token, offset)
+        println "getUpdates:" + updates
 
-def updatesJson = new JsonSlurper().parseText(updates)
+        def updatesJson = new JsonSlurper().parseText(updates)
+        if (updatesJson.result.size() > 0) {
+            def lastMsg = updatesJson.result[updatesJson.result.size() - 1]
+            if (prevLastMsg == null) {
+                prevLastMsg = lastMsg
+            }
+            if (lastMsg.message.message_id > prevLastMsg.message.message_id) {
+                println "update lastMsg:$lastMsg.text"
+                if (lastMsg.message.text.startsWith(R_CMD) == true) {
+                    def keyword = lastMsg.message.text.substring(R_CMD.length())
+                    println "keyword:$keyword"
+                    TelegramBotApiHelper.sendMessage(token, lastMsg.message.chat.id, "http://www.reddit.com/r/" + keyword)
+                }
+            } else {
+                println "no new message"
+            }
+            prevLastMsg = lastMsg
+            offset = prevLastMsg.update_id
+        }
 
-if (updatesJson.result.size() > 0) {
-    def msg = updatesJson.result[0]
-    def result = TelegramBotApiHelper.sendMessage(token, msg.message.chat.id, "hello")
-    println "result=[$result]"
+        sleep(2000)
+    }
 }
-
