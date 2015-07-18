30240ebbde9ae56264ae1a7abf56a4eac4e3a291
Thomas Min <thomas.min.git@gmail.com>
2015-07-18 21:17:08 +0900

    Add logging, expectation check.

--- http/batch.groovy
+++ http/batch.groovy
@@ -1,41 +1,62 @@
 import groovy.json.JsonSlurper
-import groovyx.net.http.ContentType
-import groovyx.net.http.Method
+import groovy.util.logging.Slf4j
 import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
+import groovyx.net.http.ContentType
 
-@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.2')
+@Grapes([
+        @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.1.3'),
+        @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.2')
+])
 
+@Slf4j
 class HttpHelper {
     static String request(options, cookies = []) {
         def method = (options.method == "post") ? Method.POST : Method.GET
-        def url = options.url
-        def path = options.path
-        def query = options.query
-
-        def http = new HTTPBuilder(url)
+        def contentType
+        switch (options.contentType) {
+            case "urlenc":
+                contentType = ContentType.URLENC
+                break
+            case "text":
+                contentType = ContentType.TEXT
+                break
+            default:
+                log.error("No content type defined")
+                return
+        }
+        def http = new HTTPBuilder(options.url)
         def ret
 
-        println "Requesting...[$url],path=[$path],query=[$query]"
-        http.request(method, ContentType.TEXT) {
-            uri.path = path
-            uri.query = query
+        log.info("Requesting...method=[$method],url=[$options.url],path=[$options.path],query=[$options.query]")
+        http.request(method, contentType) {
+            uri.path = options.path
+            if (method == Method.POST) {
+                body = options.query
+            } else {
+                uri.query = options.query
+            }
             headers.'User-Agent' = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'
             headers.'Cookie' = cookies.join(';')
             response.success = { resp, reader ->
 
-                ret = reader.text
-                println "ret=[$ret]"
-
-                println "response status: ${resp.statusLine}"
-                println 'Headers: -----------'
-                resp.headers.each { h ->
-                    println " ${h.name} : ${h.value}"
+                if (contentType == ContentType.URLENC) {
+                    ret = reader
+                    log.debug("ret=[$ret]")
+                } else {
+                    ret = reader.text
                 }
+
+//                log.debug("response status: ${resp.statusLine}")
+//                log.debug('Headers: -----------')
+//                resp.headers.each { h ->
+//                    log.debug(" ${h.name} : ${h.value}")
+//                }
 
                 if (options.useCookie == true) {
                     resp.getHeaders('Set-Cookie').each {
                         String cookie = it.value.split(';')[0]
-                        println "cookie=[$cookie]"
+//                        log.debug("cookie=[$cookie]")
                         cookies.add(cookie)
                     }
                 }
@@ -43,7 +64,7 @@ class HttpHelper {
             } // response.success
 
             response.failure = { resp, reader ->
-                println "failed $reader.text"
+                log.error("failed $reader.text")
             }
 
         } // http.request
@@ -52,12 +73,47 @@ class HttpHelper {
     }
 }
 
-def config = new JsonSlurper().parseText(new File("batch.config.json").text);
-println "requests=[$config.requests]"
+@Slf4j
+class BatchHttpClient {
+    static void main(String[] args) {
+        def config = new JsonSlurper().parseText(new File("batch.config.json").text);
+        log.debug("requests=[$config.requests]")
 
-def cookies = []
-config.requests.each { req ->
-    println "req=[$req]"
-    def ret = HttpHelper.request(req, cookies)
-    println "ret=[$ret]"
+        def cookies = []
+        config.requests.each { req ->
+            log.debug("req=[$req]")
+            def ret = HttpHelper.request(req, cookies)
+//    log.debug("ret=[$ret]")
+            if (ret != null) {
+                log.debug("ret.len=[${ret.length()}]")
+            } else {
+                log.debug("ret is null")
+            }
+            log.debug("req.response.expected=[$req.response.expected]")
+            def success = false
+            if (req.response.expected.isEmpty() == false) {
+                if (ret != null && ret.indexOf(req.response.expected) > -1) {
+                    log.info("Found expectation")
+                    success = true
+                } else {
+                    log.warn("Not matched expectation: : expected=[$req.response.expected], actual=[$ret]")
+                }
+            } else {
+                log.info("No expectation")
+                success = true
+            }
+
+            if (success == true) {
+                log.info("OK")
+            } else {
+                log.warn("Fail")
+                if (req.response.stopOnFail == true) {
+                    log.warn("Stopped")
+                    return
+                }
+            }
+        }
+    }
 }
+
+BatchHttpClient.main()
