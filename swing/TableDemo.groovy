ce6831f24269ce5c62256f23ef6d50ed3f36e259
Thomas Min <thomas.min.git@gmail.com>
2015-12-08 09:53:26 +0900

    Add TableDemo example from http://alvinalexander.com/java/jwarehouse/groovy/src/examples/groovy/swing/TableDemo.groovy.shtml.

---
+++ swing/TableDemo.groovy
@@ -0,0 +1,53 @@
+/**
+ * Demonstrates the use of the Groovy TableModels for viewing tables of any List of objects
+ */
+
+import groovy.swing.SwingBuilder
+
+class TableDemo {
+
+    // properties
+    def frame
+    def swing
+
+    static void main(args) {
+        def demo = new TableDemo()
+        demo.run()
+    }
+
+    void run() {
+        swing = new SwingBuilder()
+        swing.edt {
+            lookAndFeel 'nimbus'
+        }
+        frame = swing.frame(title:'Groovy TableModel Demo', location:[200,200], size:[300,200]) {
+            menuBar {
+                menu(text:'Help') {
+                    menuItem() {
+                        action(name:'About', closure:{ showAbout() })
+                    }
+                }
+            }
+            panel {
+                borderLayout()
+                scrollPane(constraints:CENTER) {
+                    table() {
+                        def model = [['name':'James', 'location':'London'], ['name':'Bob', 'location':'Atlanta'], ['name':'Geir', 'location':'New York']]
+
+                        tableModel(list:model) {
+                            closureColumn(header:'Name', read:{row -> return row.name})
+                            closureColumn(header:'Location', read:{row -> return row.location})
+                        }
+                    }
+                }
+            }
+        }
+        frame.show()
+    }
+
+    void showAbout() {
+        def pane = swing.optionPane(message:'This demo shows how GroovySwing can use Groovy closures to create simple table models')
+        def dialog = pane.createDialog(frame, 'About GroovySwing')
+        dialog.show()
+    }
+}
